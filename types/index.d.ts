c9a962f9d102a3aacef7ce57fe046972ce5fa183
Nathan Blair <me@nathanblair.rocks>
2020-03-16 04:27:44 -0400

    Removing typing as well
    Since there's no public API

--- types/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { Assert } from "./lib/assert.js";
-export { TestLogger } from "./lib/logger/testLogger.js";
--- types/lib/assert.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Provides an assertion for a test case
- *
- * @param {function():Promise<void>} assertFunction the assert function to call
- * @param {Boolean} skip whether to skip this assertion (doesn't run or count against total or failed assertions)
- *
- * @returns {Promise<Boolean>} a boolean indicating whether the assertion was correct
- */
-export function Assert(assertFunction: () => Promise<void>, skip: boolean): Promise<boolean>;
