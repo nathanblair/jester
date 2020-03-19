535242ed0dc87ee1899489043b252d32b263a8ec
Nathan Blair <engineer.nblair@gmail.com>
2020-03-19 19:07:44 -0400

    Can now do setUp and tearDown

--- tests/unit/testAssert.js
+++ tests/unit/testAssert.js
@@ -4,6 +4,20 @@ import { Assert } from '../../lib/assert.js'
 export const id = 'Assert Test'
 
 const dummy = 2
+let setUpVar = 4
+let setUpAsyncVar = 8
+let tearDownVar = 0
+let tearDownAsyncVar = 's'
+
+export async function setUp() {
+  setUpVar = 6
+  setUpAsyncVar = 'y'
+}
+
+export async function tearDown() {
+  tearDownVar = 10
+  tearDownAsyncVar = 'off'
+}
 
 export const assertions = {
   'Should be able to pass correct assertions': {
@@ -30,5 +44,17 @@ export const assertions = {
   },
   'Should be able to assert on module variables': {
     function: () => assert.strictEqual(dummy, 2),
-  }
+  },
+  'Should be able to setUp': {
+    function: () => assert.deepStrictEqual(setUpVar, 6)
+  },
+  'Should be able to setUp asynchronously': {
+    function: async () => assert.deepStrictEqual(setUpAsyncVar, 'y')
+  },
+  'Should be able to tearDown': {
+    function: () => assert.deepStrictEqual(tearDownVar, 10)
+  },
+  'Should be able to tearDown asynchronously': {
+    function: async () => assert.deepStrictEqual(tearDownAsyncVar, 'off')
+  },
 }
